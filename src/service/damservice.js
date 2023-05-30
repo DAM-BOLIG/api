8c594543df115f830fa397e18aef16e12aaccf25
davi5663 <davi5663@edu.sde.dk>
2023-05-30 12:07:56 +0200

    category is working...

--- src/service/damservice.js
+++ src/service/damservice.js
@@ -6,12 +6,20 @@ module.exports = (InjectedAssetDB) => {
   assetDB = InjectedAssetDB;
 
   return {
+    /* Asset requests  */
     createAsset,
     getAsset,
     deleteAsset,
     putAsset,
+    /* Category requests  */
+    getCategory,
+    createCategory,
+    deleteCategory,
+    putCategory,
   };
 };
+
+/* Asset requests  */
 
 function createAsset(req, res) {
   assetDB.createAsset(
@@ -21,8 +29,8 @@ function createAsset(req, res) {
     (response) => {
       sendResponse(
         res,
-        response.error === null ? "Succes!!" : "something, went wrong",
-        response.error
+        response.error === null ? "Succes!!" : "something, went wrong"
+        //response.error
       );
     }
   );
@@ -32,8 +40,8 @@ function getAsset(req, res) {
   assetDB.getAsset((response) => {
     sendResponse(
       res,
-      response.results.length === null ? "No Results" : response.results,
-      response.error
+      response.results.length === null ? "No Results" : response.results
+      //response.error
     );
   });
 }
@@ -42,8 +50,8 @@ function deleteAsset(req, res) {
   assetDB.deleteAsset(req.params.StyleNumber, (response) => {
     sendResponse(
       res,
-      response.error === null ? "Record Deleted" : "Something Went Wrong",
-      response.error
+      response.error === null ? "Record Deleted" : "Something Went Wrong"
+      //response.error
     );
   });
 }
@@ -52,12 +60,55 @@ function putAsset(req, res) {
   assetDB.putAsset(req.body.StyleName, req.params.StyleNumber, (response) => {
     sendResponse(
       res,
-      response.error === null ? "Record Updated" : "Something Went Wrong",
-      response.error
+      response.error === null ? "Record Updated" : "Something Went Wrong"
+      //response.error
     );
   });
 }
 
+/* Category requests  */
+
+function createCategory(req, res) {
+  assetDB.createCategory(req.body.Name, (response) => {
+    sendResponse(
+      res,
+      response.error === null ? "Succes!!" : "something, went wrong"
+      //response.error
+    );
+  });
+}
+
+function getCategory(req, res) {
+  assetDB.getCategory((response) => {
+    sendResponse(
+      res,
+      response.results.length === null ? "No Results" : response.results
+      //response.error
+    );
+  });
+}
+
+function deleteCategory(req, res) {
+  assetDB.deleteCategory(req.params.CategoryID, (response) => {
+    sendResponse(
+      res,
+      response.error === null ? "Record Deleted" : "Something Went Wrong"
+      //response.error
+    );
+  });
+}
+
+function putCategory(req, res) {
+  assetDB.putCategory(req.body.Name, req.params.CategoryID, (response) => {
+    sendResponse(
+      res,
+      response.error === null ? "Record Updated" : "Something Went Wrong"
+      //response.error
+    );
+  });
+}
+
+/* Response function  */
 function sendResponse(res, message, error) {
   res.status(error !== undefined ? 400 : 200).json({
     message: message,
